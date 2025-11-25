fd4d1b89d4ee245c294779f251ad74ead6bbfc60
zikriz <m.zikriz@gmail.com>
2025-11-25 16:56:14 +0400

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "@/react-app/index.css";
+import './index.css';
 import App from "@/react-app/App.tsx";
 
 createRoot(document.getElementById("root")!).render(
